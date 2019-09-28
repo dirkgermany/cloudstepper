f9dd6225b5c3e9ebf64e02168f722c004ac75035
Dirk <dirk@dirk-goldbach.de>
2019-09-29 00:01:02 +0200

    JobService hinzugefügt

---
+++ jobService/src/main/java/com/dam/jobService/rest/message/RestResponse.java
@@ -0,0 +1,45 @@
+package com.dam.jobService.rest.message;
+
+public class RestResponse {
+
+	private final String SERVICE_NAME = "UserService";
+
+	private Long returnCode;
+	private String description;
+	private String result;
+
+	public RestResponse(Long returnCode, String result, String description) {
+		setReturnCode(returnCode);
+		setDescription(description);
+		setResult(result);
+	}
+	
+	public String getServiceName () {
+		return SERVICE_NAME;
+	}
+
+	private void setReturnCode(Long returnCode) {
+		this.returnCode = returnCode;
+	}
+
+	public Long getReturnCode() {
+		return returnCode;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+}
