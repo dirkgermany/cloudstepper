47b3cea080a7328d277e4d88ed396ee290d3057c
Dirk <dirk@dirk-goldbach.de>
2019-11-06 01:38:29 +0100

    Symbol muss in Portfolio und Stock identisch sein

--- portfolioService/src/main/java/com/dam/portfolio/types/Symbol.java
+++ portfolioService/src/main/java/com/dam/portfolio/types/Symbol.java
@@ -5,11 +5,15 @@ public enum Symbol {
 	EXXT_DE ("EXXT.DE"),
 	EUNL_DE ("EUNL.DE"),
 	IHI ("IHI"),
+	ISN3_DE ("ISN3.DE"),
+	EXSA_DE ("EXSA.DE"),
+	SXR8_DE ("SXR8.DE"),
 	IQQE_DE ("IQQE.DE"), 
 	SXRT_DE ("SXRT.DE"),
 	BND ("BND"),
 	IBCD_DE ("IBCD.DE"),
-	IGLN_L ("IGLN.L"); 
+	IGLN_L ("IGLN.L"),
+	NA ("N/A"); 
 
 	private String requestSymbol;
 	
