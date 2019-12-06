c596dea0260f079f6d0408c979b41ea5a5c9bd67
Dirk <dirk@dirk-goldbach.de>
2019-12-06 20:26:21 +0100

    Wieder mal das CORS Problem

--- serviceProvider/src/main/java/com/dam/provider/SimpleCORSFilter.java
+++ serviceProvider/src/main/java/com/dam/provider/SimpleCORSFilter.java
@@ -37,7 +37,7 @@ public class SimpleCORSFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, Access-Control-Request-Method, Access-Control-Request-Headers, remember-me");
+		response.setHeader("Access-Control-Allow-Headers", "tokenId, Content-Type, Accept, X-Requested-With, Access-Control-Request-Method, Access-Control-Request-Headers, remember-me");
 
 		chain.doFilter(req, res);
 	}
