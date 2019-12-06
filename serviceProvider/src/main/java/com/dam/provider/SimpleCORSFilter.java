25031304ce2c80ac29865ac75fc091c203c64e8a
Dirk <dirk@dirk-goldbach.de>
2019-12-06 20:01:34 +0100

    Wieder mal das CORS Problem

--- serviceProvider/src/main/java/com/dam/provider/SimpleCORSFilter.java
+++ serviceProvider/src/main/java/com/dam/provider/SimpleCORSFilter.java
@@ -29,11 +29,11 @@ public class SimpleCORSFilter implements Filter {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
 
-//		if (null == request.getHeader("Origin") || request.getHeader("Origin").isEmpty()) {
+		if (null == request.getHeader("Origin") || request.getHeader("Origin").isEmpty()) {
 			response.setHeader("Access-Control-Allow-Origin", "https://investmal.de");
-//		} else {
-//			response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-//		}
+		} else {
+			response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+		}
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
